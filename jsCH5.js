c22208c776dd06588221ae18a1e43149f3fea915
tiarahasnabila <tiarahasnabila@gmail.com>
2024-04-09 15:53:12 +0700

    first

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click",function video(){
+    alert("ini video red velvet");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio red velvet");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar red velvet");
+});
